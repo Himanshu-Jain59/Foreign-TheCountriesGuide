6621b25908da7f29370a406d9cab004895f1aa17
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-07 21:00:12 +0530

    bg-white

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 function Country() {
   return (
     <>
-      <div className="flex border border-white w-fit rounded-md">
+      <div className="flex border border-white w-fit rounded-md bg-white">
         <div>
           <img
             src="https://flagcdn.com/w320/in.png"
             alt=""
             className="w-65 border border-white rounded-md"
           />
-          <div className="p-">
+          <div className="p-6">
             <h2 className="font-bold text-lg mb-3">Afganistan</h2>
             <p>
               <b className="font-semibold">Population: </b>40218234
