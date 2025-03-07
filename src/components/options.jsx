ec1a56de0a37d2e1f331fc206b22e8f7c37a3e5d
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-07 21:00:48 +0530

    select and styling

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -4,15 +4,19 @@ import { useState } from "react";
 function Options() {
   return (
     <>
-      <div className="flex justify-between mx-15 my-5">
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="Search for a country"
-          className="px-5 py-3 border border-black border-solid w-2/6"
-        />
-        <button>Filter</button>
+      <div className="flex justify-between mx-15 my-10 ">
+        <div className=" flex gap-4 items-centre px-5 py-3 border border-black border-solid w-2/6 bg-white">
+          <i className="fa-solid fa-magnifying-glass text-lg py-1"></i>
+
+          <input type="text" name="" id="" placeholder="Search for a country" />
+        </div>
+        <select>
+          <option value="">Filter by Region</option>
+          <option value="">Africa</option>
+          <option value="">America</option>
+          <option value="">Asia</option>
+          <option value="">Europe</option>
+        </select>
       </div>
     </>
   );
