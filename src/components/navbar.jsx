e918db4495a5e618e1f0952886971bb7dc0ed677
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-10 22:59:09 +0530

    input style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 function Navbar() {
   let [mode, setMode] = useState("Dark Mode");
   return (
-    <div className="flex justify-between py-6 px-15 border bg-white">
+    <div className="flex justify-between py-6 px-15 shadow-md bg-white">
       <h2 className="text-2xl font-bold">Where in the world?</h2>
       <button onClick={() => setMode("Light Mode")}>
-        <i class="fa-solid fa-moon"></i>
+        <i className="fa-solid fa-moon"></i>
         &nbsp;&nbsp;
         {mode}
       </button>
