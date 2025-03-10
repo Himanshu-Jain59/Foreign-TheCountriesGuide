ccbc2c6831a3fcb64a2e26e0f8e1109fbbb5a59a
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-10 22:59:29 +0530

    shadow added

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -5,10 +5,15 @@ function Options() {
   return (
     <>
       <div className="flex justify-between mx-15 my-10 ">
-        <div className=" flex gap-4 items-centre px-5 py-3 border border-black border-solid w-2/6 bg-white">
-          <i className="fa-solid fa-magnifying-glass text-lg py-1"></i>
+        <div className=" flex gap-4 items-centre px-7 py-3 rounded-lg w-2/5 bg-white shadow-md">
+          <i className="fa-solid fa-magnifying-glass text-[#858585] text-lg py-1"></i>
 
-          <input type="text" name="" id="" placeholder="Search for a country" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search for a country..."
+          />
         </div>
         <select>
           <option value="">Filter by Region</option>
