e492e73dcb98b3ca6bfcab64c828b13ed7592ecf
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-21 23:26:52 +0530

    axios

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,30 +1,55 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Country() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const res = await axios.get("https://restcountries.com/v3.1/all");
+    const result = res.data;
+    console.log(result);
+    setData(result);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <div className="flex border border-white w-fit rounded-md bg-white">
-        <div>
-          <img
-            src="https://flagcdn.com/w320/in.png"
-            alt=""
-            className="w-65 border border-white rounded-md"
-          />
-          <div className="p-6">
-            <h2 className="font-bold text-lg mb-3">Afganistan</h2>
-            <p>
-              <b className="font-semibold">Population: </b>40218234
-            </p>
-            <p>
-              <b className="font-semibold">Region: </b>Asia
-            </p>
-            <p>
-              <b className="font-semibold">Capital: </b>Kabul
-            </p>
-          </div>
-        </div>
-      </div>
+      {data.map(function (elem) {
+        return (
+          <>
+            <div
+              key={elem.flag}
+              className="flex border border-white w-fit rounded-md bg-white shadow-lg"
+            >
+              <div className="w-65">
+                <img
+                  src={elem.flags.png}
+                  alt=""
+                  className=" h-45 w-65 border border-white rounded-md "
+                />
+                <div className="p-6">
+                  <h2 className="font-bold text-lg mb-3">{elem.name.common}</h2>
+                  <p>
+                    <b className="font-semibold">Population: </b>
+                    {elem.population}
+                  </p>
+                  <p>
+                    <b className="font-semibold">Region: </b>
+                    {elem.region}
+                  </p>
+                  <p>
+                    <b className="font-semibold">Capital: </b>
+                    {elem.capital}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 }
