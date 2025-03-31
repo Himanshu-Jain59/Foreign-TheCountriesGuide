3da5689c50e2af0f6f0e5125566a7b231b0f16b1
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-31 22:04:48 +0530

    fecthing data

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link, useLocation } from "react-router-dom";
 
 function Country() {
   const [data, setData] = useState([]);
+  const { search } = useLocation();
+  // console.log(search.split("=")[1]);
 
   const getData = async () => {
-    const res = await axios.get("https://restcountries.com/v3.1/all");
+    let res = null;
+    if (search) {
+      res = await axios.get(
+        `https://restcountries.com/v3.1/region/${search.split("=")[1]}`
+      );
+    } else {
+      res = await axios.get("https://restcountries.com/v3.1/all");
+    }
     const result = res.data;
-    console.log(result);
+    // console.log(result);
     setData(result);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
 
   return (
     <>
@@ -25,11 +35,13 @@ function Country() {
               className="flex border border-white w-fit rounded-md bg-white shadow-lg"
             >
               <div className="w-65">
-                <img
-                  src={elem.flags.png}
-                  alt=""
-                  className=" h-45 w-65 border border-white rounded-md "
-                />
+                <Link to={`/detail/${elem.name.common}`}>
+                  <img
+                    src={elem.flags.png}
+                    alt=""
+                    className=" h-45 w-65 border border-white rounded-md "
+                  />
+                </Link>
                 <div className="p-6">
                   <h2 className="font-bold text-lg mb-3">{elem.name.common}</h2>
                   <p>
