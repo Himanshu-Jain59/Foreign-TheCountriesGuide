e73e4687b3e244b3cca36fb746dfb349157b74cd
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-31 22:03:58 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import "./App.css";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
+          <Route path="/detail/:name" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </>
