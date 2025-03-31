a0ff47fe811aff330ad2b4b3750dd8b822eac00f
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-31 22:05:43 +0530

    filter by region

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Options() {
+  const navigate = useNavigate();
+  const handleRegionChange = (e) => {
+    e.preventDefault();
+
+    navigate(`/?region=${e.target.value}`);
+
+    // console.log("Selected Region:", e.target.value);
+  };
   return (
     <>
-      <div className="flex justify-between mx-15 my-10 ">
+      <div className="flex justify-between mx-15 mt-1 pt-7 ">
         <div className=" flex gap-4 items-centre px-7 py-3 rounded-lg w-2/5 bg-white shadow-md">
           <i className="fa-solid fa-magnifying-glass text-[#858585] text-lg py-1"></i>
 
@@ -13,14 +21,18 @@ function Options() {
             name=""
             id=""
             placeholder="Search for a country..."
+            className="outline-0 w-full"
           />
         </div>
-        <select>
+        <select
+          className="block w-40 p-2.5 shadow-md rounded-lg bg-white text-gray-900 focus:outline-none mr-5"
+          onChange={handleRegionChange}
+        >
           <option value="">Filter by Region</option>
-          <option value="">Africa</option>
-          <option value="">America</option>
-          <option value="">Asia</option>
-          <option value="">Europe</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
         </select>
       </div>
     </>
