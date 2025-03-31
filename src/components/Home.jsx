783139e6c728709d5760b8dd1918293af9a4246f
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-31 22:05:26 +0530

    modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
-import Navbar from "./navbar";
-import Options from "./options";
-import Countries from "./countries";
+import Countries from "./Countries";
+import Navbar from "./Navbar";
+import Options from "./Options";
 
 function Home() {
   return (
     <>
-      <div className="bg-[#fafafa]">
-        <Navbar />
+      <div className="bg-[#fafafa]  ">
         <Options />
         <Countries />
       </div>
